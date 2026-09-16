f945e4d18638ffac9f88b9343a73801ab6a07a2c
Abhisek Koyal <abhisekkoyal334@gmail.com>
2026-09-16 14:14:27 +0530

    Update API base URL to include '/api' path

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://b2b-rfq-marketplace-1-gxvr.onrender.com",
+  baseURL: "https://b2b-rfq-marketplace-1-gxvr.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
