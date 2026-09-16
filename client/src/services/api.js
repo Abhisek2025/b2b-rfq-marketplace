00fb43ad4971bab04fcf7245e2b9969f484783c3
Abhisek Koyal <abhisekkoyal334@gmail.com>
2026-09-16 12:33:10 +0530

    Change API base URL to production environment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://b2b-rfq-marketplace-t0by.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
